ff9b397d68c66af9e0db1a8141181b08086ea2d4
Jonathan <sifreed77@gmail.com>
2018-01-25 23:34:32 -0500

    Updated groupby.sql

--- DML/select/groupby.sql
+++ DML/select/groupby.sql
@@ -1,4 +1,4 @@
-select count(c.id), c.fname, c.lname, c.salary, c.department from customers c
+select count(c.id), c.fname, c.salary, c.department from customers c
 where c.salary > 60000
 group by c.department
 having count(c.id) > 10;
