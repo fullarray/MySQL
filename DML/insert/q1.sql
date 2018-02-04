64044d78d1422229d9b9d1b534bf805b71fcebcb
Jonathan <sifreed77@gmail.com>
2018-02-03 23:49:01 -0500

    Added insert queries implementation

---
+++ DML/insert/q1.sql
@@ -0,0 +1,4 @@
+use mainDatabase;
+
+INSERT INTO customer (fname, lname, title, address, city, st, country)
+VALUES ('John', 'Doe', 'Software Developer', '123 Las Olas Blvd', 'Sunrise', 'FL', 'US');
---
+++ DML/insert/q2.sql
@@ -0,0 +1,4 @@
+use mainDatabase;
+
+INSERT INTO customer (fname, lname, title, address, city, st, country)
+VALUES ('Marcos', 'Frodo', 'Project Manager', '23 N University Dr.', 'Lauderdale Lakes', 'FL', 'US');
