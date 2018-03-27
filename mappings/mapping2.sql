73f40d279ce104e45b0abb5b7a39c4d849a85f20
Jonathan <sifreed77@gmail.com>
2018-03-27 00:22:34 -0400

    Added mapping2 implementation

---
+++ mappings/mapping2.sql
@@ -0,0 +1,34 @@
+create database animal_rights_campaign;
+
+use animal_rights_campaign;
+
+create table animal(
+	animal_id INT(11) AUTO_INCREMENT,
+    animal_name VARCHAR(50),
+    animal_age INT(11),
+    registry_id INT(11),
+    PRIMARY KEY(animal_id)
+);
+
+create table zoo(
+	zoo_id INT AUTO_INCREMENT,
+    zoo_name VARCHAR(50),
+    animal_id INT(11),
+	FOREIGN KEY(animal_id)
+		REFERENCES enrollment (animal_id)
+		ON DELETE cascade
+		ON UPDATE cascade,
+	zoo_id INT(11),
+    FOREIGN KEY(zoo_id)
+		REFERENCES enrollment (animal_id)
+		ON DELETE cascade
+		ON UPDATE cascade,
+	PRIMARY KEY(zoo_id)
+);
+
+create table registry(
+	registry_id INT AUTO_INCREMENT PRIMARY KEY,
+    date_aquired date
+);
+
+
