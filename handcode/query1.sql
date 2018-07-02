ab8ee3c619b2fc3d663e8febba868e2e17d36b72
Jonathan <sifreed77@gmail.com>
2018-07-01 23:53:40 -0400

    Update query1.sql

--- handcode/query1.sql
+++ handcode/query1.sql
@@ -3,13 +3,13 @@ create database student;
 use student;
 
 create table student_signups(
-	student_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    student_id INT(11) AUTO_INCREMENT PRIMARY KEY,
     student_name VARCHAR(50),
     student_age INT(11)
 );
 
 create table course(
-	course_id INT AUTO_INCREMENT PRIMARY KEY,
+    course_id INT AUTO_INCREMENT PRIMARY KEY,
     course_name VARCHAR(50),
     enroll_id INT(11),
     FOREIGN KEY(enroll_id)
@@ -19,6 +19,6 @@ create table course(
 );
 
 create table enrollement(
-	enrollment_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    enrollment_id INT(11) AUTO_INCREMENT PRIMARY KEY,
     semester_name VARCHAR(50)
 );
