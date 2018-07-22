58f60f4861e3c75f7ca6602d0402e47a78774a03
Jonathan <sifreed77@gmail.com>
2018-07-21 23:06:05 -0400

    Update q2.sql

--- DML/insert/q2.sql
+++ DML/insert/q2.sql
@@ -1,4 +1,3 @@
 use mainDatabase;
-
 INSERT INTO customer (fname, lname, title, address, city, st, country)
 VALUES ('Marcos', 'Frank', 'Project Manager', '23 N University Dr.', 'Lauderdale Lakes', 'FL', 'US');
