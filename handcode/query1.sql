da287df730548844b30ecd978b4129e82f186992
Jonathan <sifreed77@gmail.com>
2018-08-10 23:04:23 -0400

    Update query1.sql

--- handcode/query1.sql
+++ handcode/query1.sql
@@ -16,6 +16,18 @@ create table course(
 		ON UPDATE cascade
 );
 
+
+create table coursetype(
+    coursetype_id INT AUTO_INCREMENT PRIMARY KEY,
+    course_type VARCHAR(50),
+    enroll_id INT(11),
+    FOREIGN KEY(enroll_id)
+		REFERENCES enrollment (enrollment_id)
+		ON DELETE cascade
+		ON UPDATE cascade
+);
+
+
 create table enrollement(
     enrollment_id INT(11) AUTO_INCREMENT PRIMARY KEY,
     semester_name VARCHAR(50)
