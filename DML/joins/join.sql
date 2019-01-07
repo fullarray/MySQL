18bc91d6f926c9001dd4fd0c12874341444bd881
Jonathan <sifreed77@gmail.com>
2019-01-06 22:57:33 -0500

    Update join.sql

--- DML/joins/join.sql
+++ DML/joins/join.sql
@@ -17,3 +17,11 @@ join courselist i on c.courseid = i.id
 join cohort h on s.id = h.id
 where s.id = 6;
 
+select s.studentid, c.grade, i.name coursename, h.cohort, h.semester 
+from student s
+join course c on s.id = c.studentid
+join courselist i on c.courseid = i.id
+join cohort h on s.id = h.id
+where s.id = 8;
+
+
