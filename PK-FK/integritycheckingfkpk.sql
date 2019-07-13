6aff4e5dffe8a643d8418098c18fc39c734acee1
Jonathan <sifreed77@gmail.com>
2019-07-12 23:13:13 -0400

    Update integritycheckingfkpk.sql

--- PK-FK/integritycheckingfkpk.sql
+++ PK-FK/integritycheckingfkpk.sql
@@ -4,7 +4,7 @@
 --first. Therefore, integrity check will not allow
 --the task to continue unless both tables are
 --deleted at the same time. Concurrent, command
---executions are not possible.
+--executions are not possibles.
 SET FOREIGN_KEY_CHECKS = 0;
 drop table if exists customers;
 drop table if exists invoices;
